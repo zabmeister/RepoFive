abaa1790d10a09e33d01e6d0f0019558bbc4e3d9
unknown <zabmesiter@gmail.com>
2012-06-28 01:59:26 +0100

    Added MVC 3 subscriber (not working - not received messages, no ui for it)

---
+++ Subscriber.Mvc/Controllers/SubscriberController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Subscriber.Mvc.Controllers
+{
+    public class SubscriberController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new ContentResult { Content = "Hello world" };
+        }
+    }
+}
---
+++ Subscriber.Mvc/NewListHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+using Messages;
+
+namespace Subscriber.Console.EventHandlers
+{
+    public class NewListHandler : IHandleMessages<NewList>
+    {
+        public void Handle(NewList message)
+        {
+            //NOTE: note sure how to do this in MVC
+            System.Console.WriteLine("Message received: {0}", message.Source);
+        }
+    }
+}
