5a2f157554b5d07a67624dc3e0c53f91a9958378
unknown <zabmesiter@gmail.com>
2012-06-28 02:46:30 +0100

    Added Wpf project (not received messages)

---
+++ Subscriber.Wpf/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using NServiceBus;
+
+namespace Subscriber.Wpf
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            NServiceBus.Configure.With()
+             .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("Messages"));
+        }
+    }
+}
---
+++ Subscriber.Wpf/ViewModels/NewListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+using Messages;
+using System.Collections.ObjectModel;
+
+namespace Subscriber.Wpf
+{
+    public class NewListViewModel : IHandleMessages<NewList>
+    {                
+        public ObservableCollection<string> Items { get; private set; }
+
+        public NewListViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        public void Handle(NewList message)
+        {
+            Items.Add(message.Source);
+        }
+    }
+}
